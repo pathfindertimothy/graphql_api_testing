2e61484e427d9fefb7ab6f79ee3df314df2ca4ae
timothy.tonwuru@gig.com <timothy.tonwuru@gig.com>
2024-04-26 14:31:59 +0200

    Ref06: 1. Updated git ignore file

--- auth_params/auth_params.js
+++ auth_params/auth_params.js
@@ -1,8 +1,13 @@
-// provide your token: remove the curly brace and provide token
-const token = '{provide token}';
+// // provide your token: remove the curly brace and provide token
+// const token = '{provide token}';
 
-// provide your bearer_token: remove the curly brace and provide token
-const bearer_token = 'Bearer {provide bear_token}';
+// // provide your bearer_token: remove the curly brace and provide token
+// const bearer_token = 'Bearer {provide bear_token}';
+
+// provide your token
+const token = 'BQY6y8mDwhwud2uBsJEynwZJ9wVfMC47';
+// provide your bearer_token
+const bearer_token = 'Bearer ory_at_lVC8dqKts8FDLp5OLYWZEYVXolrwU8tyvk54hxR_rWk.hbbtKqo01rM7N12MBBr7ncvINX5pRAZYJC-llkjkjMs';
 
 const url = 'https://streaming.bitquery.io/graphql';
 const requestHeaders = {
